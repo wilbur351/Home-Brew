e0297ac29b3132510c15319f66d599693c68880e
Walter Schmidt <walter.j.schmidt@gmail.com>
2012-01-31 00:50:40 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeBrew::Application.config.secret_token = '3eabaa4ad076dc2046e2913db98aab22937c998f65ba0b3702d2fdafd620802aa1f9041b0d5a08ffc975989bc19fc485651ae9994f82f5aca6e8cd9e0f7800ae'
